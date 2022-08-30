8aa3aba985e8bf30b38f0931e21f087d1fbc4d0c
Taylor Henderson <tph5595@verizon.net>
2022-08-30 08:10:16 -0400

    WIP

--- src/barcode.rs
+++ src/barcode.rs
@@ -1,4 +1,5 @@
 use crate::birthdeath::BirthDeath;
+use std::collections::BinaryHeap;
 
 #[derive(Debug)]
 enum EventType {
@@ -7,35 +8,61 @@ enum EventType {
 }
 
 #[derive(Debug)]
+struct Node {
+    birth_event: Event,
+    death_event: Event,
+    id: usize,
+    alive: bool,
+    in_top_k: bool,
+    is_dead: bool,
+}
+
+fn get_value(n: Node) -> Event {
+    match n.alive {
+        true => n.death_event,
+        false => n.birth_event,
+    }
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        (get_value(self).value, self.id).cmp(get_value(other).value, other.id)
+    }
+}
+
+#[derive(Debug)]
 struct Event {
     event_type: EventType,
     value: f32,
-    id: usize,
 }
 
-fn generate_events(bd_pairs: Vec<BirthDeath>) -> Vec<BirthDeath> {
-    let _tmp = bd_pairs.into_iter().enumerate().map(|(i, bd)| match bd {
-        BirthDeath { birth, death } if death.is_finite() && birth.is_finite() => {
-            vec![
-                Event {
-                    event_type: EventType::Birth,
-                    value: birth,
-                    id: i,
-                },
-                Event {
-                    event_type: EventType::Death,
-                    value: death,
-                    id: i,
-                },
-            ]
-        }
-        _ => Vec::new(),
-    });
-    return Vec::new();
+fn create_event(birth: f32, death: f32, i: usize) -> Node {
+    return Node {
+        birth_event: Event {
+            event_type: EventType::Birth,
+            value: birth,
+        },
+        death_event: Event {
+            event_type: EventType::Death,
+            value: death,
+        },
+        id: i,
+        alive: false,
+        in_top_k: false,
+        is_dead: false,
+    };
+}
+
+fn generate_events(bd_pairs: Vec<BirthDeath>) -> Vec<Node> {
+    return bd_pairs
+        .into_iter()
+        .filter(|BirthDeath { birth, death }| death.is_finite() && birth.is_finite())
+        .enumerate()
+        .map(|(i, BirthDeath { birth, death })| create_event(birth, death, i))
+        .collect::<Vec<_>>();
 }
 
 pub fn barcode_filter(bd_pairs: Vec<BirthDeath>, k: i32) -> Vec<BirthDeath> {
     let events = generate_events(bd_pairs);
-
-    return events;
+    let mut eventStack = BinaryHeap::from(events);
 }
